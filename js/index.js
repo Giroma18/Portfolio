b76ef8f0816021c5316a904e3b632eecdda1048c
Valérie Giroux-Martone <P1469417@hotmail.com>
2025-11-13 10:28:14 -0500

    modification
    Modification de la struture

--- js/index.js
+++ js/index.js
@@ -86,13 +86,4 @@ window.addEventListener("DOMContentLoaded", gererAffichageContenuSections);
 			}
 		}
 
-const barreMenu = document.querySelector('.barre-menu');
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
-      barreMenu.classList.add('petit');
-    } else {
-      barreMenu.classList.remove('petit');
-    }
-  });
 
